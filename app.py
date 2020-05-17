cbb3ecb7c0d61cce440efdf1516024bba60fd9bd
Jatin Kaushik <jatinkaushik@Jatins-MacBook-Air.local>
2020-05-17 08:27:31 +0530

    manager.py removed

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+# from flask_script import Manager
+from app import app
+
+# manager = Manager(app)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
+    # manager.run()
