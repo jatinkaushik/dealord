1c57e8756a58ffb18421a720bd09d5495b2f6648
Jatin Kaushik <jatinkaushik@Jatins-MacBook-Air.local>
2020-05-17 08:49:09 +0530

    minor changes

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://flask:bhuvnesh@curesee.in/flask'
 app.config['SECRET_KEY'] = 'Dealord'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 from app.User import *
