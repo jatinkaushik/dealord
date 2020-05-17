5462dd341fbc2c2f0d6711f433a4ef3796eda5a5
Jatin Kaushik <jatinkaushik@Jatins-MacBook-Air.local>
2020-05-17 09:10:36 +0530

    added hello world

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask import render_template, flash, request, url_for, redirect, session, jsonify
 # from flask_script import Manager
 from app import app
 
 # manager = Manager(app)
 
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
