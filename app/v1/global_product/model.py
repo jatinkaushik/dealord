27e1db3e67dc0f6bc841a511783718c1866dd90f
neerajkaushik05 <neerajkaushik05@gmail.com>
2020-08-27 22:25:43 +0530

    Model_Change saved

--- app/v1/global_product/model.py
+++ app/v1/global_product/model.py
@@ -24,7 +24,7 @@ class GlobalProductCategory(db.Model):
 # class GlobalProductFeaturesDatatype(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
 #     name = db.Column(db.String(20))
-#     category_features_rel = db.relationship('GlobalProductCategoryFeature', backref='general_data_datatype')
+#     category_feature_rel = db.relationship('GlobalProductCategoryFeature', backref='general_data_datatype')
 
 
 class GlobalProductFeaturesGroups(db.Model):
